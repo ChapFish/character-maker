b740d6ab8d69831229ab7db5519d936c8e4358a3
ChapFish <tech.octo@gmail.com>
2018-03-27 18:50:01 +0900

    ステータス表示画面、スタート画面とダイスロール、技能設定画面の一部を作成。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { DiceRollComponent } from './dice-roll/dice-roll.component';
+import { SearcherService } from './searcher.service';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { StatusViewComponent } from './status-view/status-view.component';
+import { AbilityComponent } from './ability/ability.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiceRollComponent,
+    SidebarComponent,
+    StatusViewComponent,
+    AbilityComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [SearcherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
